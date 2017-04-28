da67af7b21092c7a87a3fcd01604c44307f1fa75
Сергей Прыгунов <sergey1q2w3e@gmail.com>
2017-04-28 12:26:30 +0300

    add MapPoint

--- MapRunner/MainPage.xaml.cs
+++ MapRunner/MainPage.xaml.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.ServiceModel.Channels;
 using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Services.Maps;
+using Windows.Storage.Streams;
 using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Maps;
@@ -16,26 +21,32 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
 namespace MapRunner
 {
-    /// <summary>
-    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private RandomAccessStreamReference _mapIconStreamReference;
+        public List<PointOfCustomRoute> PointList { get; set; }
+        
         public MainPage()
         {
             this.InitializeComponent();
+            _mapIconStreamReference = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/pin48.png"));
             // Check if Streetside is supported.
             //Show();
             //ShowRouteOnMap();
+            GetLocation();
+            PointList = new List<PointOfCustomRoute>();
+            myMap.DataContext = this;
         }
 
         private async void ShowRouteOnMap()
         {
+            
             // Start at Microsoft in Redmond, Washington.
             BasicGeoposition startLocation = new BasicGeoposition() { Latitude = 47.643, Longitude = -122.131 };
 
@@ -98,26 +109,71 @@ namespace MapRunner
             }
         }
 
-        private void HamdurgerButton_Click(object sender, RoutedEventArgs e)
+        private async void GetLocation()
         {
-            if (MainSplitView.IsPaneOpen)
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            switch (accessStatus)
             {
-                MainSplitView.IsPaneOpen = false;
-                btnDarkLightMode.Width = 40;
-                btnDarkLightMode.HorizontalAlignment = HorizontalAlignment.Left;
-                btnMapMode.Width = 40;
-                btnMapMode.HorizontalAlignment = HorizontalAlignment.Left;
-            }
-            else
-            {
-                MainSplitView.IsPaneOpen = true;
-                btnDarkLightMode.Width = 150;
-                btnDarkLightMode.HorizontalAlignment = HorizontalAlignment.Stretch;
-                btnMapMode.Width = 150;
-                btnMapMode.HorizontalAlignment = HorizontalAlignment.Stretch;
+                case GeolocationAccessStatus.Allowed:
+                    Debug.WriteLine("Waiting for update...");
+
+                    // If DesiredAccuracy or DesiredAccuracyInMeters are not set (or value is 0), DesiredAccuracy.Default is used.
+                    Geolocator geolocator = new Geolocator { DesiredAccuracyInMeters = 10 };
+
+                    // Subscribe to the StatusChanged event to get updates of location status changes.
+                   // _geolocator.StatusChanged += OnStatusChanged;
+
+                    // Carry out the operation.
+                    Geoposition pos = await geolocator.GetGeopositionAsync();
+                    Debug.WriteLine("Location updated.");
+                    myMap.Center = pos.Coordinate.Point;
+                    myMap.TrySetViewAsync(pos.Coordinate.Point, 16, myMap.Heading, myMap.DesiredPitch, MapAnimationKind.Linear);
+                    MapIcon mapIcon1 = new MapIcon();
+                    mapIcon1.Location = myMap.Center;
+                    mapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+                    mapIcon1.Title = "My Position";
+                    mapIcon1.Image = _mapIconStreamReference;
+                    mapIcon1.ZIndex = 0;
+                    myMap.MapElements.Remove(mapIcon1);
+                    myMap.MapElements.Add(mapIcon1);
+                    
+                    break;
+
+                case GeolocationAccessStatus.Denied:
+                    Debug.WriteLine("Access to location is denied.");
+                    
+                    
+                    break;
+
+                case GeolocationAccessStatus.Unspecified:
+                    Debug.WriteLine("Unspecified error.");
+                    
+                    break;
             }
         }
 
+
+        private void HamdurgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            SplitPanelRefresh(MainSplitView.IsPaneOpen);
+            MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
+        }
+
+        private void SplitPanelRefresh(bool isOpen)
+        {
+            if (isOpen)
+            {
+                btnDarkLightMode.Width = btnMapMode.Width = btnGetLocation.Width = btnNewRoute.Width = 40;
+                btnDarkLightMode.HorizontalAlignment =
+                    btnMapMode.HorizontalAlignment = btnGetLocation.HorizontalAlignment = btnNewRoute.HorizontalAlignment = HorizontalAlignment.Left;
+            }
+            else
+            {
+                btnDarkLightMode.Width = btnMapMode.Width = btnGetLocation.Width = btnNewRoute.Width = 150;
+                btnDarkLightMode.HorizontalAlignment =
+                    btnMapMode.HorizontalAlignment = btnGetLocation.HorizontalAlignment = btnNewRoute.HorizontalAlignment = HorizontalAlignment.Stretch;
+            }
+        }
         private void btnDarkLightMode_Click(object sender, RoutedEventArgs e)
         {
             if (myMap.ColorScheme == MapColorScheme.Light)
@@ -142,9 +198,67 @@ namespace MapRunner
                     myMap.Style = MapStyle.Terrain;
                     break;
                 case MapStyle.Terrain:
+                    myMap.Style = MapStyle.AerialWithRoads;
+                    break;
+                case MapStyle.AerialWithRoads:
                     myMap.Style = MapStyle.Aerial;
                     break;
             }
+        }
+
+        private void btnGetLocation_Click(object sender, RoutedEventArgs e)
+        {
+            GetLocation();
+        }
+
+        private void MainSplitView_PaneClosed(SplitView sender, object args)
+        {
+            SplitPanelRefresh(true);
+        }
+        
+        private void btnNewRoute_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+
+
+        private void myMap_MapTapped(MapControl sender, MapInputEventArgs args)
+        {
+            Debug.WriteLine(String.Format(args.Location.Position.Latitude +" " +args.Location.Position.Longitude));
+            PointOfCustomRoute currPoint = new PointOfCustomRoute()
+            {
+                Location = args.Location,
+                NormalizedAnchorPoint = new Point(0.5, 0.93)
+            };
+            if (PointList.Count != 0)
+            {
+                currPoint.CurrentLenght = PointOfCustomRoute.GetDistance(PointList[PointList.Count - 1], currPoint);
+            }
+            PointList.Add(currPoint);
+
+            MapPolyline polyline = new MapPolyline();
+            polyline.StrokeColor = Colors.Navy;
+            polyline.StrokeThickness = 3;
+            List<BasicGeoposition> geopositions = new List<BasicGeoposition>();
+            foreach (PointOfCustomRoute pointOfCustomRoute in PointList)
+            {
+                geopositions.Add(pointOfCustomRoute.Location.Position);
+            }
+            
+            polyline.Path = new Geopath(geopositions);
+            if (myMap.MapElements.Contains(polyline))
+            {
+                myMap.MapElements.Remove(polyline);
+            }
+            myMap.MapElements.Add(polyline);
+            //if(PointList.Count == 4)
+            //    MapItems.ItemsSource = PointList;
+
+            MapItems.ItemsSource = null;
+            MapItems.ItemsSource = PointList;
+            
+
         }
     }
 }
---
+++ MapRunner/PointOfCustomRoute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls.Maps;
+
+namespace MapRunner
+{
+    public class PointOfCustomRoute
+    {
+        private const int RADIUS_EARTH = 6372795; //радиус Земли в метрах
+        public int Count { get; set; }
+        public Geopoint Location { get; set; }
+        public double CurrentLenght { get; set; }
+        public Point NormalizedAnchorPoint { get; set; }
+
+        public PointOfCustomRoute()
+        {
+
+            
+
+        }
+
+        public static double GetDistance(PointOfCustomRoute p1, PointOfCustomRoute p2)
+        {
+            //Y=atan{sqrt(a+(b-c)^2)/(d+e)}
+            double lat1 = p1.Location.Position.Latitude * Math.PI / 180;
+            double long1 = p1.Location.Position.Longitude * Math.PI / 180;
+            double lat2 = p2.Location.Position.Latitude * Math.PI / 180;
+            double long2 = p2.Location.Position.Longitude * Math.PI / 180;
+            double a = Math.Pow(Math.Cos(lat2) * Math.Sin(long2 - long1), 2);
+            double b = Math.Cos(lat1) * Math.Sin(lat2);
+            double c = Math.Sin(lat1) * Math.Cos(lat2) * Math.Cos(long2 - long1);
+            double d = Math.Sin(lat1) * Math.Sin(lat2);
+            double e = Math.Cos(lat1) * Math.Cos(lat2) * Math.Cos(long2 - long1);
+            double Y = Math.Atan(Math.Sqrt(a + Math.Pow(b - c, 2)) / (d + e));
+
+            return RADIUS_EARTH * Y;
+        }
+    }
+}
